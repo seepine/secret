c8c289ee723634d108113e15af6000ef83e58606
seepine <seepine@outlook.com>
2022-10-07 23:29:43 +0800

    set redisson codec default jackson

--- secret-quarkus/runtime/src/main/java/com/seepine/secret/quarkus/runtime/AppInit.java
+++ secret-quarkus/runtime/src/main/java/com/seepine/secret/quarkus/runtime/AppInit.java
@@ -1,11 +1,16 @@
 package com.seepine.secret.quarkus.runtime;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seepine.secret.AuthUtil;
 import com.seepine.secret.interfaces.TokenParser;
 import com.seepine.secret.properties.AuthProperties;
 import com.seepine.secret.util.RedisUtil;
 import io.quarkus.runtime.StartupEvent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
 
 import javax.enterprise.event.Observes;
 
@@ -15,7 +20,10 @@ import javax.enterprise.event.Observes;
  * @author seepine
  */
 public class AppInit {
+  private static final String JACKSON_CODEC = "org.redisson.codec.JsonJacksonCodec";
 
+  @ConfigProperty(name = "quarkus.redisson.codec", defaultValue = JACKSON_CODEC)
+  String redissonCodec;
   /**
    * @param event event
    * @param redissonClient redis
@@ -23,10 +31,17 @@ public class AppInit {
   void startup(
       @Observes StartupEvent event,
       RedissonClient redissonClient,
+      ObjectMapper mapper,
       AuthProperties authProperties,
       TokenParser tokenParser) {
     if (redissonClient != null) {
-      RedisUtil.init(redissonClient);
+      if (JACKSON_CODEC.equals(redissonCodec)) {
+        Config config = redissonClient.getConfig();
+        config.setCodec(new JsonJacksonCodec(mapper));
+        RedisUtil.init(Redisson.create(config));
+      } else {
+        RedisUtil.init(redissonClient);
+      }
     }
     AuthUtil.init(authProperties, tokenParser);
   }
