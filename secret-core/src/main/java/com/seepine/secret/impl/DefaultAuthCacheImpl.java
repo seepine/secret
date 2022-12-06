e5b9c5aa950c98a7ff3708c7bb6084d15b20ff18
seepine <seepine@outlook.com>
2022-12-07 00:03:58 +0800

    improvement(core/impl): improvement default cache logic

--- secret-core/src/main/java/com/seepine/secret/impl/DefaultAuthCacheImpl.java
+++ secret-core/src/main/java/com/seepine/secret/impl/DefaultAuthCacheImpl.java
@@ -3,58 +3,31 @@ package com.seepine.secret.impl;
 import com.seepine.secret.entity.AuthUser;
 import com.seepine.secret.interfaces.AuthCache;
 import com.seepine.secret.properties.AuthProperties;
-import com.seepine.secret.util.CurrentTimeMillis;
-import com.seepine.secret.util.RedisUtil;
-
-import java.util.HashMap;
+import com.seepine.secret.util.CacheUtil;
 
 /**
  * @author seepine
  * @since 0.0.6
  */
 public class DefaultAuthCacheImpl implements AuthCache {
-  private final boolean hasRedisson;
   private final AuthProperties authProperties;
-  private final HashMap<String, AuthUser> spareDb;
 
   public DefaultAuthCacheImpl(AuthProperties authProperties) {
-    this.hasRedisson = !RedisUtil.isShutdown();
     this.authProperties = authProperties;
-    spareDb = new HashMap<>();
   }
 
   @Override
-  @SuppressWarnings("unchecked")
   public <T extends AuthUser> T get(String key) {
-    if (hasRedisson) {
-      return RedisUtil.get(key);
-    }
-    AuthUser user = spareDb.get(key);
-    if (user != null) {
-      // if now-lastRefreshTime > timeout
-      if (CurrentTimeMillis.now() - user.getRefreshTime() > authProperties.getTimeout()) {
-        remove(key);
-        return null;
-      }
-    }
-    return (T) user;
+    return CacheUtil.get(key);
   }
 
   @Override
   public <T extends AuthUser> void set(String key, T val) {
-    if (hasRedisson) {
-      RedisUtil.set(key, val);
-    } else {
-      spareDb.put(key, val);
-    }
+    CacheUtil.set(key, val, authProperties.getTimeout() * 1000);
   }
 
   @Override
   public void remove(String key) {
-    if (hasRedisson) {
-      RedisUtil.remove(key);
-    } else {
-      spareDb.remove(key);
-    }
+    CacheUtil.remove(key);
   }
 }
