e4b8235b6bd01eef8528824e842e2232251482d0
seepine <seepine@outlook.com>
2023-01-13 00:26:10 +0800

    fix(quarkus): fix auto config property

--- secret-quarkus/runtime/src/main/java/com/seepine/secret/quarkus/runtime/config/AutoConfig.java
+++ secret-quarkus/runtime/src/main/java/com/seepine/secret/quarkus/runtime/config/AutoConfig.java
@@ -1,20 +1,29 @@
 package com.seepine.secret.quarkus.runtime.config;
 
 import com.seepine.secret.properties.AuthProperties;
-import com.seepine.secret.quarkus.runtime.properties.AuthPropertiesImpl;
+import com.seepine.secret.quarkus.runtime.properties.SecretProperties;
 import io.quarkus.arc.DefaultBean;
+import org.eclipse.microprofile.config.inject.ConfigProperties;
 
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 /**
  * @author seepine
  */
 public class AutoConfig {
+  @Inject @ConfigProperties SecretProperties secretProperties;
+
   @Produces
   @DefaultBean
   @Singleton
   public AuthProperties authProperties() {
-    return new AuthPropertiesImpl();
+    AuthProperties authProperties = new AuthProperties();
+    authProperties.setExpiresAt(secretProperties.expiresAt);
+    authProperties.setSecret(secretProperties.secret);
+    authProperties.setCachePrefix(secretProperties.cachePrefix);
+    authProperties.setIssuer(secretProperties.issuer);
+    return authProperties;
   }
 }
---
+++ secret-quarkus/runtime/src/main/java/com/seepine/secret/quarkus/runtime/properties/SecretProperties.java
@@ -0,0 +1,23 @@
+package com.seepine.secret.quarkus.runtime.properties;
+
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+/**
+ * @author seepine
+ */
+@ConfigProperties(prefix = "secret")
+public class SecretProperties {
+  /** token过期时间，单位秒，默认12小时 */
+  @ConfigProperty(name = "expires-at", defaultValue = "43200")
+  public Long expiresAt;
+  /** 缓存前缀 */
+  @ConfigProperty(name = "cache-prefix", defaultValue = "com.seepine.secret:")
+  public String cachePrefix;
+  /** 默认jwt加密密钥 */
+  @ConfigProperty(defaultValue = "com.seepine.secret")
+  public String secret;
+  /** 默认jwt的issuer */
+  @ConfigProperty(defaultValue = "secret")
+  public String issuer;
+}
