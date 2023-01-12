62c84d6aff6d4c9b01c8c6b9bec433780ef6a7a0
seepine <seepine@outlook.com>
2023-01-12 20:05:09 +0800

    test(quarkus): remove test

--- secret-quarkus/deployment/src/test/java/com/seepine/secret/quarkus/test/SecretQuarkusTest.java
+++ secret-quarkus/deployment/src/test/java/com/seepine/secret/quarkus/test/SecretQuarkusTest.java
@@ -1,31 +1,31 @@
-package com.seepine.secret.quarkus.test;
-
-import com.seepine.secret.quarkus.runtime.properties.AuthPropertiesImpl;
-import io.quarkus.test.QuarkusUnitTest;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
-
-import javax.inject.Inject;
-
-public class SecretQuarkusTest {
-
-  // Start unit test with your extension loaded
-  @RegisterExtension
-  static final QuarkusUnitTest unitTest =
-      new QuarkusUnitTest().setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
-
-  @Inject AuthPropertiesImpl authProperties;
-
-  //  @Test
-  //  public void writeYourOwnUnitTest() {
-  //     Write your unit tests here - see the testing extension guide
-  // https://quarkus.io/guides/writing-extensions#testing-extensions for more information
-  //    Assertions.assertTrue(true, "Add some assertions to " + getClass().getName());
-  //  }
-  @Test
-  public void testProperties() {
-    System.out.println(authProperties);
-  }
-}
+// package com.seepine.secret.quarkus.test;
+//
+// import com.seepine.secret.quarkus.runtime.properties.AuthPropertiesImpl;
+// import io.quarkus.test.QuarkusUnitTest;
+// import org.jboss.shrinkwrap.api.ShrinkWrap;
+// import org.jboss.shrinkwrap.api.spec.JavaArchive;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.RegisterExtension;
+//
+// import javax.inject.Inject;
+//
+// public class SecretQuarkusTest {
+//
+//  // Start unit test with your extension loaded
+//  @RegisterExtension
+//  static final QuarkusUnitTest unitTest =
+//      new QuarkusUnitTest().setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+//
+//  @Inject AuthPropertiesImpl authProperties;
+//
+//  //  @Test
+//  //  public void writeYourOwnUnitTest() {
+//  //     Write your unit tests here - see the testing extension guide
+//  // https://quarkus.io/guides/writing-extensions#testing-extensions for more information
+//  //    Assertions.assertTrue(true, "Add some assertions to " + getClass().getName());
+//  //  }
+//  @Test
+//  public void testProperties() {
+//    System.out.println(authProperties);
+//  }
+// }
