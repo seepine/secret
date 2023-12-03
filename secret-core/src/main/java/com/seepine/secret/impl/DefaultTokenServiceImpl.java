4696e7704b86397bb9d4e37b9b36a5b5eca44d8c
seepine <seepine@outlook.com>
2023-12-03 23:22:25 +0800

    fix(core): add sign verify exception catch

--- secret-core/src/main/java/com/seepine/secret/impl/DefaultTokenServiceImpl.java
+++ secret-core/src/main/java/com/seepine/secret/impl/DefaultTokenServiceImpl.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
@@ -128,6 +129,8 @@ public class DefaultTokenServiceImpl implements TokenService {
       throw e;
     } catch (TokenExpiredException e) {
       throw new ExpiresSecretException(e.getExpiredOn().getEpochSecond());
+    } catch (SignatureVerificationException e) {
+      throw new TokenSecretException();
     } catch (Exception e) {
       throw new TokenSecretException(e);
     }
